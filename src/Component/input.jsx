e4d31eb7ac3629aadd36be0b15174a4c988c5445
Sachin Mahto <sachin@edvenswatech.com>
2024-08-02 15:52:47 +0530

    Code updated with all functionality

--- src/Component/input.jsx
+++ src/Component/input.jsx
@@ -1,34 +1,37 @@
 import React from "react";
-export default function input({
-   type,
-   placeholder,
-   currencyOptions=[],
-   className,
-   set,
-   fromTo
-})
-  {
-    const handleChange=((e)=>{
-      set(e.target.value)
-    })
-    return(
-        
-        <div>
-              <label htmlFor={type} className='block text-white mb-2'>{type}</label>
-              <select name={type} className={className} onChange={handleChange}>
-              <option value="" disabled selected>{placeholder}</option>
-                {
-                   currencyOptions.map((currency)=>{
-                    return(
-                        <option key={currency} 
-                        value={fromTo}
-                        >{currency}</option>
-                      )
-                   })
-                    
-                }
-              </select>
-        </div>
-      
-    )
+
+export default function Input({
+  type,
+  placeholder,
+  currencyOptions = [],
+  className,
+  set,
+  fromTo
+}) {
+  const handleChange = (e) => {
+    set(e.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor={type} className='block text-white mb-2'>
+        {type}
+      </label>
+      <select
+        name={type}
+        className={className}
+        onChange={handleChange}
+        value={fromTo} 
+      >
+        <option value="" disabled>
+          {placeholder}
+        </option>
+        {currencyOptions.map((currency) => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
